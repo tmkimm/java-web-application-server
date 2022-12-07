ee402071abd705679f3e92ae2dfe3f57b1833e12
tmkimm <xoals1417@gmail.com>
2022-12-07 15:39:06 +0900

    HTML/CSS 요청 처리

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,24 +18,53 @@ public class RequestHandler extends Thread {
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader((new InputStreamReader(in)));
+            String line;
+            ArrayList<String> requests = new ArrayList<String>();
+            while(true) {
+                line = br.readLine();
+                if(line == null || line.isEmpty())
+                    break;
+                requests.add(line);
+            }
+            log.debug("New Client Request! Url : {}", requests.get(0));
+            int firstBlank = requests.get(0).indexOf(" ") + 1;
+            String url = requests.get(0).substring(firstBlank, requests.get(0).indexOf(" ", firstBlank));
+
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            String contentType = "text/html";
+            byte[] body;
+            if(url.indexOf(".html") != -1) {
+                body = Files.readAllBytes(new File("./webapp"+ url).toPath());
+            } else if(url.indexOf(".css") != -1) {
+                body = Files.readAllBytes(new File("./webapp"+ url).toPath());
+                contentType = "text/css";
+            }else {
+                body = "Hello World".getBytes();
+            }
+            response200Header(dos, body.length, contentType);
             responseBody(dos, body);
+            // InputStream 첫라인 읽어서 컨트롤러 만들기
+            // favicon.ico 제외
+
+            // 서빙하도록
+            // GET /index.html?id=1234 HTTP/1.1
+            // GET / HTTP/1.1
+
+            // 첫 스페이스 찾아서 GET
+            // /뒤에 문자열 잘라서 경로
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + contentType + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
