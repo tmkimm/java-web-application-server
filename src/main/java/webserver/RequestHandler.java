c0b09dc1edbb411a0eaa10b31bfe1a8780c659ca
tmkimm <xoals1417@gmail.com>
2022-12-09 13:13:46 +0900

    refactor: ArrayList > HashMap, POST Body read

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,17 +6,21 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import jdk.internal.util.xml.impl.Input;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import repository.MemoryUserRepository;
+import util.HttpRequestUtils;
 import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private static HashMap<String, User> store = new HashMap<String, User>();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -24,68 +28,83 @@ public class RequestHandler extends Thread {
 
     public void run() {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            ArrayList<String> requests = toHttpRequest(in);
-            String url = getUrl(requests.get(0));
-            User user = null;
-            // 쿼리 파라미터 파싱
-            HashMap<String, String> queryParams = new HashMap<>();
-            if(url.indexOf("?") != -1) {
-                queryParams = getQueryParams(url.substring(url.indexOf("?") + 1));
-                log.debug("query parm:"+queryParams);
-            }
-            // 컨트롤러로 refactoring
+            HashMap<String, String> requests = toHttpRequestHashMap(in);
+
             DataOutputStream dos = new DataOutputStream(out);
             String contentType = "text/html";
+            String url = requests.get("URL");
             byte[] body;
-            if(url.indexOf(".html") != -1) {
-                body = Files.readAllBytes(new File("./webapp"+ url).toPath());
-            } else if(url.indexOf(".css") != -1) {
-                body = Files.readAllBytes(new File("./webapp"+ url).toPath());
-                contentType = "text/css";
-            } else if(url.indexOf("/user/create") != -1) {
+
+            if(url.startsWith("GET /user/create")) {   // GET 방식 회원가입
                 body = "create success!".getBytes();
-                user = new User(queryParams.get("userId"), queryParams.get("password"), queryParams.get("name"), queryParams.get("email"));
-                log.debug(user.toString());
-            } else if(url.indexOf("/user/login") != -1) {
+                HashMap<String, String> queryParams = getQueryParams(url);  // 쿼리 파라미터 파싱
+                User user = new User(queryParams.get("userId"), queryParams.get("password"), queryParams.get("name"), queryParams.get("email"));
+                store.put(queryParams.get("userId"), user);
+            }
+            else if(url.startsWith("POST /user/create")) { // POST 방식 회원가입
+                body = "create success!".getBytes();
+                Map<String, String> bodyMap = HttpRequestUtils.parseQueryString(requests.get("Body"));
+                User user = new User(bodyMap.get("userId"), bodyMap.get("password"), bodyMap.get("name"), bodyMap.get("email"));
+                store.put(bodyMap.get("userId"), user);
+            }
+            else if(url.startsWith("GET /user/login")) { // 로그인
                 body = "login page".getBytes();
-                if(user != null)
-                    log.debug(user.toString());
-            }else {
+                log.debug(store.toString());
+            }
+            else if(url.indexOf(".html") != -1) {
+                body = Files.readAllBytes(new File("./webapp"+ parseUrl(url)).toPath());
+            } else if(url.indexOf(".css") != -1) {
+                body = Files.readAllBytes(new File("./webapp"+ parseUrl(url)).toPath());
+                contentType = "text/css";
+            } else {
                 body = "Hello World".getBytes();
             }
-
             response200Header(dos, body.length, contentType);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
-    public ArrayList<String> toHttpRequest(InputStream in) {
-        ArrayList<String> requests = new ArrayList<String>();
+
+    public HashMap<String, String> toHttpRequestHashMap(InputStream in) {
+        HashMap<String, String> requests = new HashMap<>();
+        String line;
         try {
             BufferedReader br = new BufferedReader((new InputStreamReader(in)));
-            String line;
+            // URL Setting
+            line = br.readLine();
+            requests.put("URL", line);
+
+            // HTTP Headers Setting
             while(true) {
                 line = br.readLine();
-                if((line == null || line.isEmpty()))
+                if(line == null || line.isEmpty())
                     break;
-                requests.add(line);
+                String[] keyValue = line.split(": ");
+                requests.put(keyValue[0], keyValue[1]);
+            }
+
+            // HTTP Body Setting
+            if(requests.get("URL").startsWith("POST")) {
+                String requestBody = IOUtils.readData(br, Integer.parseInt(requests.get("Content-Length")));
+                requests.put("Body", requestBody);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        log.debug("New Request >>  Url : {}", requests.get(0));
         return requests;
     }
-    public String getUrl(String text) {
+    public String parseUrl(String text) {
         int firstBlank = text.indexOf(" ") + 1;
         String url = text.substring(firstBlank, text.indexOf(" ", firstBlank));
         return url;
     }
-
     public HashMap<String, String> getQueryParams(String url) {
+        if(url.indexOf("?") == -1) {
+            return null;
+        }
         HashMap<String, String> queryParams = new HashMap<>();
-        String[] splitArr = url.split("&");
+        String[] splitArr = url.substring(url.indexOf("?") + 1).split("&");
         String[] item;
         for(String v: splitArr) {
             item = v.split("=");
